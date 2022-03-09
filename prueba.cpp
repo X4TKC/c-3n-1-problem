d36350ef91f81234d94ef809601449393fd05679
X4tkc <tovilaskevin@gmail.com>
2022-03-09 19:27:58 -0400

    fix 0.0.2

--- prueba.cpp
+++ prueba.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 asdasdas
+asdaint
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
      int x,y;
+     int z,q;
     // int x,y;
     // cin >> x >> y;
     // string line;
